53b4be859970b68e8b561c5403025a424720baf2
Nicola Pompini <nicola.pompini@gmail.com>
2022-07-09 10:16:03 +0200

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,55 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand navbar-dark bosco-dark">
-      <div class="navbar-nav mr-auto">
-        <li class="navbar-brand">
-          <router-link to="/home" class="nav-link">
-            <font-awesome-icon icon="tree" /> Critter
-          </router-link>
-        </li>
+    <div id="content-wrap">
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand navbar-dark bosco-dark">
+        <div class="navbar-nav mr-auto">
+          <li class="navbar-brand">
+            <router-link to="/home" class="nav-link">
+              <font-awesome-icon icon="tree" /> Critter
+            </router-link>
+          </li>
+        </div>
+        <div v-if="!currentUser" class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <router-link to="/register" class="nav-link">
+              <font-awesome-icon icon="seedling" /> Sign Up
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link">
+              <font-awesome-icon icon="carrot" /> Login
+            </router-link>
+          </li>
+        </div>
+        <div v-else class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <router-link to="/profile" class="nav-link">
+              <font-awesome-icon icon="paw" /> {{ currentUser.username }}
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" @click.prevent="logOut">
+              <font-awesome-icon icon="suitcase" /> Logout
+            </a>
+          </li>
+        </div>
+      </nav>
+
+      <!-- Content -->
+      <div class="container">
+        <router-view />
+      </div> 
+    </div>
+
+    <!-- Footer -->
+    <footer>
+      <div class="text-center p-2 my-footer font-italic">
+        &copy; 2022 Copyright: ThousandPines<span style="font-size:0.9em"> srl</span>
+        <br/> All rights reserved
       </div>
-      <div v-if="!currentUser" class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <router-link to="/register" class="nav-link">
-            <font-awesome-icon icon="seedling" /> Sign Up
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/login" class="nav-link">
-            <font-awesome-icon icon="carrot" /> Login
-          </router-link>
-        </li>
-      </div>
-      <div v-else class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <router-link to="/profile" class="nav-link">
-            <font-awesome-icon icon="paw" /> {{ currentUser.username }}
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" @click.prevent="logOut">
-            <font-awesome-icon icon="suitcase" /> Logout
-          </a>
-        </li>
-      </div>
-    </nav>
-    <div class="container">
-      <router-view />
-    </div> 
+    </footer>
+
   </div>
 </template>
 
@@ -59,9 +73,24 @@ export default {
 <style lang="scss">
 @import "./styles/settings.scss";
 
-#app, body, html {
+#app {
+  position: relative;
+  min-height: 100vh;
   background-color: $boscoLight;
-  min-height: 100%;
+}
+
+#content-wrap {
+  padding-bottom: 3.8rem;
+}
+
+footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 3.8rem;
+  background-color: $boscoSoftDark;
+  font-size: 0.9em;
+  color: rgba(255, 255, 255, 0.7);
 }
 
 .bosco-dark {
